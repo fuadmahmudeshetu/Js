df5326df36df24ccd69e7b289d4e2b7babe93567
Fuad <fuadmahmud0303@gmail.com>
2025-01-20 22:06:34 +0200

    e

---
+++ Bro Js/do while loop.js
@@ -0,0 +1,13 @@
+
+//do while loops
+// let username;
+
+// do{
+//     username= window.prompt("Enter your name");
+// }while(username=="");
+
+//     console.log("Hello", username);
+
+// for loop
+
+
