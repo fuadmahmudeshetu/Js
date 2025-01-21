ab1bb89dad8d27ecba8e941ed49769b7dd6be1ed
Fuad <fuadmahmud0303@gmail.com>
2025-01-21 21:22:45 +0200

    jj

--- Bro Js/do while loop.js
+++ Bro Js/do while loop.js
@@ -10,8 +10,14 @@
 
 // for loop
 
-for(let counter=1; counter<=15;counter+=5){
-    console.log(counter);
-}
+// for(let counter=1; counter<=15;counter+=5){
+//     console.log(counter);
+// }
 
-console.log("Happy new year");
+// console.log("Happy new year");
+
+for (let i = 1; i <= 3; i+=1){
+    for (let j = 1; j <= 3; j += 1) {
+        console.log(j);
+    }
+}
