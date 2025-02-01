e0804065153b7621d07522727bd92b7bdd093335
Fuad <fuadmahmud0303@gmail.com>
2025-02-01 09:39:20 +0200

    Practice class,asynchronous

---
+++ Bro Js/Love.java
@@ -0,0 +1,11 @@
+public class Love {
+    public static void main(String[] args) {
+        love();
+    }
+
+    static void love(){
+        for (int i = 0; i <= 1000; i++) {
+            System.out.println("I Love You");
+        }
+    }
+}
--- Bro Js/class.js
+++ Bro Js/class.js
@@ -193,15 +193,34 @@ fish.eat();
 
 //get == binds an object property to a function when that property is accessed
 
-class Car{
-    Car(power){
-        this._power=power;
-    }
-   get power(){
-    return this._power;
-   }
-}
+// class Car{
+//     constructor(power){
+//         this._gas= 25;
+//         this._power= power;
+//     }
+//     get power(){
+//         return `${this._power}hp`;
+//     }
 
-let car = new Car(400);
+//     get gas(){
+//         return `${this._gas}L (${this._gas/50*100}%)`;
+//     }
+//     set gas(value){
+//         if(value>50){
+//             value = 50;
+//         }
+//         else if(value < 0){
+//             value = 0;
+//         }
+//         this._gas = value;
+//     }
+// }
 
-console.log(car.power);
+// let car = new Car(400);
+
+// car.gas = 10000000000;
+// console.log(car.power);
+// console.log(car.gas);
+
+
+
