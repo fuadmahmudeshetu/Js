3bd3d41c3d74b99b98239736b0b0a1dee81c6f77
Fuad <fuadmahmud0303@gmail.com>
2025-02-17 09:33:10 +0300

    window

---
+++ Bro Js/window.js
@@ -0,0 +1,10 @@
+
+// console.dir(window);
+// console.log(window.innerWidth);
+// console.log(window.innerHeight);
+
+// console.log(window.scrollX);
+// console.log(window.scrollY);
+
+console.log(window.location.href);
+window.location.href = "https://xnxx.com";
